82895f50297457e7fc433ce514c4be6251732f58
BLACKSHADOW827 <73574779+BLACKSHADOW827@users.noreply.github.com>
2021-02-04 17:38:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+
+var star
+var fairy
+var backgroud
+var star
+var engine, world;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+
+
+
+function preload(){ 
+  fairy27=loadAnimation("images/fairy1.png","images/fairy2.png")
+   //preload the images here
+   backgroud7=loadImage("images/starnight.png")
+   star7=loadImage("images/star.png")
+}
+
+function setup () {
+  
+  
+  createCanvas(800,750);
+  backgroud=createSprite(400,375,20,20)
+  backgroud.addImage("hha",backgroud7)
+  fairy=createSprite(600,550,50,50)
+  fairy.addAnimation("fryz",fairy27)
+  fairy.scale=0.3
+  star=createSprite(650,100,20,20)
+  star.addImage("hsgur",star7)
+  star.scale=0.4
+  engine=Engine.create();
+  world=engine.world;
+  
+  var star_options ={
+    isStatic: true
+  }
+    
+  
+    starBody = Bodies.rectangle(6500,100,20,20,star_options)
+  
+    World.add(world,star);
+
+  
+  
+  
+  
+  
+
+
+
+}
+
+
+
+
+function draw() {
+  background("black");
+  Engine.update(engine)
+  if(keyDown("space")){
+    star.velocityY=10
+     
+    }
+    if(keyDown("right")){
+      fairy.x=fairy.x+3
+      }
+      if(keyDown("left")){
+        fairy.x=fairy.x-3
+        }
+        if(star.y>515){
+          star.velocityY=0
+        }
+        
+        starBody.x=star.x
+  starBody.y=star.y
+  
+  
+  
+  
+ drawSprites();
+}
